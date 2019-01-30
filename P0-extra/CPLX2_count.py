49c771669ff3f29b48cbb93ac9616a72dcb63a1f
anaguil <46930470+anaguil@users.noreply.github.com>
2019-01-30 11:37:21 +0100

    Third version

--- P0-extra/CPLX2_count.py
+++ P0-extra/CPLX2_count.py
@@ -9,13 +9,8 @@ with open(file) as f:
         if line.startswith(">"):
             pass
         else:
-            counter_a = line.count("A")
-            count_a += counter_a
-            counter_c = line.count("C")
-            count_c += counter_c
-            counter_g = line.count("G")
-            count_g += counter_g
-            counter_t = line.count("T")
-            count_t += counter_t
-    print("The number of bases in this file is: \n A: ", count_a, "\nC: ", count_c, "\nG: ", count_g, "\nT: ", count_t)
-
+            count_a += line.count("A")
+            count_c += line.count("C")
+            count_g += line.count("G")
+            count_t += line.count("T")
+    print("The number of bases in this file is:\nA:", count_a, "\nC:", count_c, "\nG:", count_g, "\nT:", count_t)
