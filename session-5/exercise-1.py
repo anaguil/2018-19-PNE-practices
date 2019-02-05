97e69a94156b273e98e2b7d41285e0323a9c174c
anaguil <anuski.7.2000@gmail.com>
2019-02-05 16:45:10 +0100

    Counting number of bases and percentage

---
+++ session-5/exercise-1.py
@@ -0,0 +1,38 @@
+def count_bases(seq):
+    """Counting the number of As in the sequence"""
+    count_a = 0
+    count_c = 0
+    count_g = 0
+    count_t = 0
+    dictionary_bases = dict()
+    for b in seq:
+        if b == "A":
+            count_a += 1
+        elif b == "C":
+            count_c += 1
+        elif b == "G":
+            count_g += 1
+        elif b == "T":
+            count_t += 1
+    dictionary_bases = {"Base A": count_a, "Base C": count_c, "Base G": count_g, "Base T": count_t}
+
+    # Return the result
+    return dictionary_bases
+
+
+# Main program
+s = input("Please enter the sequence: ")
+dictionary_bases = count_bases(s)
+# Calculate the total length
+tl = len(s)
+
+print("The total length is: {} bases".format(tl))
+
+for key, value in dictionary_bases.items():
+    print(key)
+    print("  Counter:", value)
+    if tl > 0:
+        perc = round(100 * value / tl, 1)
+    else:
+        perc = 0
+    print("  Percentage: {}".format(perc))
