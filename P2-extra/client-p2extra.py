9797f953887a35d72f657feb6c30f4ec97f032e5
anaguil <46930470+anaguil@users.noreply.github.com>
2019-02-13 19:09:51 +0100

    Client that sends a sequence to the server and recieves the complement from it

---
+++ P2-extra/client-p2extra.py
@@ -0,0 +1,20 @@
+# Creating a client that sends a sequence to the server and retrieves the complement donde by the server
+
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Configuration of the server and connecting to it
+PORT = 8080
+IP = "212.128.253.64" #this is the IP of the computer that is working as a client
+s.connect((IP, PORT))
+
+sequence = input("Enter the sequence : ")
+
+# Send the sequence to the server
+s.send(str.encode(sequence))
+
+# Recieve the complement of the sequence from the server
+msg = s.recv(2048).decode("utf-8")
+print(msg)
+s.close()
