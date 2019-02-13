2da27ba45bc709e23fa84140ca1982f90f40f08d
anaguil <46930470+anaguil@users.noreply.github.com>
2019-02-13 17:31:29 +0100

    Update to insert everything into the loop

--- session-7/client-exercise3.py
+++ session-7/client-exercise3.py
@@ -2,16 +2,16 @@
 
 import socket
 
-# Create a socket to connect with internet (AF_INET) and with the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-PORT = 8080
-IP = "212.128.253.80"
-
-# Connect to the server
-s.connect((IP, PORT))
-
-# Sending info to the server but in bytes. Must transform the string into bytes
 while True:
+    # Create a socket to connect with internet (AF_INET) and with the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    PORT = 8080
+    IP = "212.128.253.80"
+
+    # Connect to the server
+    s.connect((IP, PORT))
+    
+    # Sending info to the server but in bytes. Must transform the string into bytes
     msg = input("Enter a message:")
     s.send(str.encode(msg))
     s.close()
