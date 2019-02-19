7d6a30bab29eb4b35b7d0557f3666619d3c31121
anaguil <anuski.7.2000@gmail.com>
2019-02-19 16:47:46 +0100

    Echo server that works with termcolor

--- session-9/server-exercise1.py
+++ session-9/server-exercise1.py
@@ -4,7 +4,7 @@ import termcolor
 PORT = 8080
 IP = "212.128.253.77"
 MAX_OPEN_REQUEST = 5
-
+loop = True
 
 def process_client(cs):
     """Function to read the info of the client and process it"""
@@ -12,12 +12,17 @@ def process_client(cs):
     # Reading the message from the client. We decode to transform from bytes to string
     msg = cs.recv(2048).decode("utf-8")
 
-    print("Message from the client: {}".format(termcolor.cprint(msg, 'yellow')))
+    if msg == "EXIT":
+        loop = False
+        cs.close()
 
-    # Sending the message back to the client. We encode from string to bytes
-    cs.send(str.encode("Connection finished"))
+    else:
+        print("Message from the client: ")
+        termcolor.cprint(msg, 'yellow')
 
-    # Optional: you can close it here or in the main loop - here you write: cs.close()
+        # Sending the message back to the client. We encode from string to bytes
+        cs.send(str.encode(msg))
+        cs.close()
 
 
 # Create a socket to connect to the clients (internet, type of socket created (type string))
@@ -31,7 +36,7 @@ serversocket.listen(MAX_OPEN_REQUEST)
 
 print("Socket ready: {}", format(serversocket))
 
-while True:
+while loop == True:
     print("Waiting for connections at: {}, {}".format(IP, PORT))
 
     # Method that will block the server and wait until the client is connected,
@@ -42,6 +47,3 @@ while True:
     print("Attending client: {}".format(address))
 
     process_client(clientsocket)
-
-    # -- Close the socket
-    clientsocket.close()
