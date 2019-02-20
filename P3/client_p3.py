bf4a62c6facdac139ef828f1f8b797c2e072c16d
anaguil <46930470+anaguil@users.noreply.github.com>
2019-02-20 20:49:47 +0100

    Client that sends a sequence and operations to a server

--- P3/client_p3.py
+++ P3/client_p3.py
@@ -2,20 +2,15 @@ import socket
 
 # SERVER IP, PORT
 IP = "212.128.253.86"
-PORT = 8081
+PORT = 8080
 
-
-msg = "AG\nlen"
+msg = """AGCTACGT\nlen\ncomplement\ncountA\ncountC"""
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((IP, PORT))
 s.send(str.encode(msg))
 
-# Receive the servers response
+# Receive the servers response and print it
 response = s.recv(2048).decode()
-
-# Print the server's response
 print("Response: {}".format(response))
-
-
 s.close()
