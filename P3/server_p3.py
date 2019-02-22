128ab6cd8eede4f45835addb144be8a2d73f256f
anaguil <anuski.7.2000@gmail.com>
2019-02-22 18:50:33 +0100

    Server that works with a sequence and operates with it

--- P3/server_p3.py
+++ P3/server_p3.py
@@ -2,7 +2,7 @@ import socket
 from seq import Seq
 
 PORT = 8080
-IP = "212.128.253.86"
+IP = "212.128.253.104"
 MAX_OPEN_REQUEST = 5
 
 # Create an empty list to add the messages from the server to the client
@@ -19,27 +19,20 @@ def process_client(cs):
     msg = cs.recv(2048).decode("utf-8")
     
     # Obtain from the client's message a list with the parts of the message
-    msg = msg.split("\n")
-    if msg[0] == "":
+    l_msg = msg.split("\n")
+    if l_msg[0] == "":
         cs.send(str.encode("Alive"))
         cs.close()
     else:
-        counter = 0
-        # If the sequence is not valid, send ERROR
-        for base in msg[0]:
+        for base in l_msg[0].upper():
             if base != "A" and base != "C" and base != "G" and base != "T":
-                counter += 1
-        if counter > 0:
-            cs.send(str.encode("ERROR"))
-            cs.close()
-        # If the sequence is valid, start with the operations
-        else:
+                cs.send(str.encode("ERROR"))
+                cs.close()
+        if len(l_response) == 0:
             l_response.append("OK")
-            # Transform the sequence from the client into class Seq
-            sequence = Seq(msg[0])
-            # Remove from the list the actual sequence to work with the other parameters
-            msg = msg.remove(msg[0])
-            for elem in msg:
+            sequence = Seq(l_msg[0].upper())
+            l_msg.remove(l_msg[0])
+            for elem in l_msg:
                 if elem == "len":
                     l_response.append(str(sequence.len()))
                 elif elem == "complement":
