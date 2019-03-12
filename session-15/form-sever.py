5cf474ab092b0f13c8b0034a17f09f373270e65a
anaguil <anuski.7.2000@gmail.com>
2019-03-12 16:59:18 +0100

    Session 15

---
+++ session-15/form-sever.py
@@ -0,0 +1,40 @@
+import http.server
+import socketserver
+import termcolor
+
+PORT = 8001
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # -- Printing the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        f = open("form1.html", 'r')
+        contents = f.read()
+
+        self.send_response(200)
+
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+        # -- Sending the body of the response message
+        self.wfile.write(str.encode(contents))
+
+
+# -- Main program
+# Empty address means that it is your own (localhost)
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Stopped by the user")
+
+print("The server is stopped")
---
+++ session-15/server-exercise1.py
@@ -0,0 +1,56 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+
+        # -- Printing the request line
+
+        f = open("form1.html", 'r')
+        contents = f.read()
+
+        message = self.path
+        i = message.find("=")
+        message = message[i+1:]
+        print(message)
+        html_text_1 = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Response</title>
+    </head>
+    <body>
+    Echo received:<br>"""
+        html_text_2 = """<p>{}</p>""".format(message)
+        html_text_3 = """<p><a href="form1.html">[Main Page]</a></body></html>"""
+        f = open("response-page.html", 'w')
+        f.write(html_text_1 + html_text_2 + html_text_3)
+        f.close()
+        f = open("response-page.html", 'r')
+        f.close()
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Content-Length', len(str.encode(contents)))
+        self.end_headers()
+
+        # -- Sending the body of the response message
+        self.wfile.write(str.encode(contents))
+
+
+# -- Main program
+# Empty address means that it is your own (localhost)
+
+with socketserver.TCPServer(("", PORT), TestHandler) as httpd:
+    print("Serving at PORT: {}".format(PORT))
+
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print("Stopped by the user")
+
+print("The server is stopped")
