3caa79f847b8d7a43f2da14980e4e6186c17cee5
anaguil <anuski.7.2000@gmail.com>
2019-03-13 18:49:40 +0100

    Practice 6

--- P6/server-p6.py
+++ P6/server-p6.py
@@ -1,12 +1,14 @@
 import http.server
 import socketserver
-from seq import Seq
+from seq_p6 import Seq
 
 PORT = 8080
+
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
+        yes = True
         print("Path:", self.path)
 
         if self.path == "/" or self.path.startswith("/seq"):
@@ -14,15 +16,57 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             contents = f.read()
 
         elif self.path.startswith("/echo"):
-            message = self.path
-            i = message.find("=")
-            message = message[i + 1:]
-            for base in message.upper():
+            list_message = self.path.split("&")
+            sequence = list_message[0]
+            i = sequence.find("?")
+            sequence = sequence[i+5:].upper()
+            for base in sequence.upper():
                 if base != "A" and base != "C" and base != "G" and base != "T":
                     f = open("error.html")
                     contents = f.read()
-                else:
-                    ######
+                    yes = False
+                    f.close()
+            if yes == True:
+                sequence = Seq(sequence)
+                i_operation = self.path.find("operation=") + len("operation=")
+                if 'chk' in self.path:
+                    seq_len = sequence.len()
+                    html_text_3 = """<p>Length: {}</p>""".format(seq_len)
+                if 'count' in self.path:
+                    j_operation = i_operation + len("count")
+                    operation = self.path[i_operation:j_operation]
+                    result_op = sequence.count(self.path[-1])
+                if 'perc' in self.path:
+                    j_operation = i_operation + len("perc")
+                    operation = self.path[i_operation:j_operation]
+                    result_op = sequence.perc(self.path[-1])
+
+                html_text_1 = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Response</title>
+    </head>
+    <body>"""
+                html_text_2 = """<p>Sequence: {}</p>""".format(sequence.strbases)
+                html_text_4 = """<p>Operation and base requested: {}, {} </p>""".format(operation, self.path[-1])
+                html_text_5 = """<p>Result of the operation: {}</p>""".format(result_op)
+                html_text_6 = """</body></html>"""
+                f = open("response-page.html", 'w')
+
+                f.write(html_text_1 + html_text_2)
+                if 'chk' in self.path:
+                    f.write(html_text_3)
+                f.write(html_text_4 + html_text_5 + html_text_6)
+                f.close()
+
+                f = open("response-page.html", 'r')
+                contents = f.read()
+                f.close()
+        else:
+            f = open("error_1.html", 'r')
+            contents = f.read()
+            f.close()
 
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
